2d4e857e3404064f91e25c2b703c501cff4dafbb
Andrey Averyanov <averyanov1996@gmail.com>
2022-03-18 23:45:08 +0300

    Commit

---
+++ MVVM/CourseDetails/CourseDetailsViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  CourseDetailsViewModel.swift
+//  MVVM
+//
+//  Created by Андрей Аверьянов on 18.03.2022.
+//
+
+import Foundation
+
+protocol CourseDetailsViewModelProtocol {
+    var courseName: String { get }
+    init(course: Course)
+}
+
+class CourseDetailsViewModel: CourseDetailsViewModelProtocol {
+    
+}
