38396818da503db561586aa037fc4a65f58a30e0
Andrey Averyanov <averyanov1996@gmail.com>
2022-03-18 23:39:41 +0300

    Commit

---
+++ MVVM/CourseDetails/CourseDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CourseDetailsViewController.swift
+//  MVVM
+//
+//  Created by Андрей Аверьянов on 18.03.2022.
+//
+
+import UIKit
+
+class CourseDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+
+}
